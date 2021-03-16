f9ea3d5d93e3740477bfa02cf51c6241f319a11c
Venika <venika.jarvis@gmail.com>
2021-03-15 23:31:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Nika";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+
+let excitedWelcomeMessage =  "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
